fb356492c241e193e9c38c0cb784421f7d0756f2
shinshin86 <beagles1986@gmail.com>
2020-09-10 08:12:42 +0900

    Fix type

--- examples/nextjs-with-typescript/pages/_app.tsx
+++ examples/nextjs-with-typescript/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { TransProvider } from 'use-mini18n';
 import i18n from '../i18n';
+import type { AppProps } from 'next/app';
 
-export default function MyApp({ Component, pageProps }: any) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <TransProvider i18n={i18n}>
       <Component {...pageProps} />
