102333a2c19ad2e2485cdfc62f57d9e1de87b625
shinshin86 <beagles1986@gmail.com>
2020-09-12 18:18:17 +0900

    Add basic test(minimal)

--- __test__/use-mini18n.test.tsx
+++ __test__/use-mini18n.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { TransProvider, useI18n } from '../dist/use-mini18n';
 
 const i18n = {
@@ -12,25 +12,100 @@ const i18n = {
   },
 };
 
-const App = (): JSX.Element => {
-  const { t, lang } = useI18n();
-
-  return (
-    <div>
-      <p>{t['hello world']}</p>
-      <p>Selected lang: {lang}</p>
-    </div>
-  );
-};
-
 describe('use-mini18n', () => {
-  test('test', () => {
-    render(
-      <TransProvider i18n={i18n}>
-        <App />
-      </TransProvider>
-    );
+  const App = (): JSX.Element => {
+    const { t, lang, langs, changeLang } = useI18n();
 
-    screen.debug();
+    return (
+      <div>
+        <h1>{t['hello world']}</h1>
+        <p>Selected lang: {lang}</p>
+        <select
+          name="lang"
+          onChange={({ target }) => changeLang(target.value)}
+          value={lang}
+        >
+          {langs.map((l, i) => (
+            <option value={l} key={i}>
+              {l}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+
+  describe('defaultLang test', () => {
+    test('Not specify defaultLang (en)', () => {
+      render(
+        <TransProvider i18n={i18n}>
+          <App />
+        </TransProvider>
+      );
+
+      expect(screen.getByText('hello world')).toBeInTheDocument();
+      expect(screen.getByText('Selected lang: en')).toBeInTheDocument();
+    });
+
+    test('Specify defaultLang (ja)', () => {
+      render(
+        <TransProvider i18n={i18n} defaultLang="ja">
+          <App />
+        </TransProvider>
+      );
+
+      expect(screen.getByText('こんにちは 世界')).toBeInTheDocument();
+      expect(screen.getByText('Selected lang: ja')).toBeInTheDocument();
+    });
+  });
+
+  describe('function of changeLang test', () => {
+    test('Change lang (en -> ja)', () => {
+      render(
+        <TransProvider i18n={i18n} defaultLang="en">
+          <App />
+        </TransProvider>
+      );
+
+      expect(screen.getByText('hello world')).toBeInTheDocument();
+      expect(screen.getByText('Selected lang: en')).toBeInTheDocument();
+
+      fireEvent.change(screen.getByRole('combobox'), {
+        target: { value: 'ja' },
+      });
+
+      expect(screen.getByText('こんにちは 世界')).toBeInTheDocument();
+      expect(screen.getByText('Selected lang: ja')).toBeInTheDocument();
+    });
+
+    test('Change lang (ja -> en)', () => {
+      render(
+        <TransProvider i18n={i18n} defaultLang="ja">
+          <App />
+        </TransProvider>
+      );
+
+      expect(screen.getByText('こんにちは 世界')).toBeInTheDocument();
+      expect(screen.getByText('Selected lang: ja')).toBeInTheDocument();
+
+      fireEvent.change(screen.getByRole('combobox'), {
+        target: { value: 'en' },
+      });
+
+      expect(screen.getByText('hello world')).toBeInTheDocument();
+      expect(screen.getByText('Selected lang: en')).toBeInTheDocument();
+    });
+  });
+
+  describe('error test', () => {
+    test('Invalid i18n', () => {
+      expect(() => {
+        render(
+          <TransProvider i18n={''}>
+            <App />
+          </TransProvider>
+        );
+      }).toThrow();
+    });
   });
 });
