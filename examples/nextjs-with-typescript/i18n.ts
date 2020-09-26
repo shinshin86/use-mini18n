353d36a3e1853dd86e58ec150faf9bb0beb422ac
shinshin86 <beagles1986@gmail.com>
2020-09-26 21:59:32 +0900

    Update example

--- examples/nextjs-with-typescript/i18n.ts
+++ examples/nextjs-with-typescript/i18n.ts
@@ -1,9 +1,11 @@
 const i18n: any = {
   en: {
     'Hello Next.js': 'Hello Next.js',
+    'Hello someone': 'Hello {name1} and {name2}',
   },
   ja: {
     'Hello Next.js': 'こんにちは Next.js',
+    'Hello someone': 'こんにちは {name1} と {name2}',
   },
 };
 
--- examples/nextjs-with-typescript/pages/index.tsx
+++ examples/nextjs-with-typescript/pages/index.tsx
@@ -2,11 +2,14 @@ import Layout from '../components/Layout';
 import { useI18n } from 'use-mini18n';
 
 const IndexPage = () => {
-  const { t, lang, langs, changeLang } = useI18n();
+  const { t, lang, langs, changeLang, getText } = useI18n();
 
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>{t['Hello Next.js']} 👋</h1>
+      <h2>
+        {getText('Hello someone', { name1: 'TestUser1', name2: 'TestUser2' })}
+      </h2>
       <p>Selected Language: {lang}</p>
       <select
         name="lang"
